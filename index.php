60e44965a6974bdfb7fb1b687fb666112268338f
Pedro Justo <pedrojusto1470@gmail.com>
2024-10-09 20:59:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estoque</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUa6mO3i7vafqONpsEX65BR5oTOZkSbr7TxKCkAo1uHBR9tTIBz0TsmrG5eD" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    
+
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="login-form">
+                    <h4 class="text-center mb-4">Login</h4>
+                    <form>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email:</label>
+                            <input type="email" class="form-control" id="email" placeholder="Digite seu email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Senha:</label>
+                            <input type="password" class="form-control" id="password" placeholder="Digite sua senha" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100">Entrar</button>
+                    </form>
+                    <p class="text-center mt-3">Esqueceu sua senha? <a href="#">Recuperar</a></p>
+                    <p class="text-center mt-2">Não tem uma conta? <a href="#">Cadastre-se</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-QDtENHFI4UldDgt2zRbojoXfY62tD40egbwtTkKp4mCIcAKr/smrh94OlLL1rxdi" crossorigin="anonymous"></script>
+</body>
+</html>
